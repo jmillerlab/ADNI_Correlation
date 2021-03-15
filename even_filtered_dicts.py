054892f81099b6de442d835ac5493233185b44fd
Erik Huckvale <erikhuck@gmail.com>
2021-03-15 16:03:40 -0600

    Makes the even-filtered-dicts script spread the comparisons across more files to lower even more the amount of resources required for the col-comparison-subset script

--- even_filtered_dicts.py
+++ even_filtered_dicts.py
@@ -9,13 +9,13 @@ from math import ceil
 
 from utils import iterate_filtered_dicts, get_comp_key, BONFERRONI_ALPHA, MAXIMUM_ALPHA
 
-N_DICTS: int = 1294
+N_OUTPUT_DICTS: int = 4000
 LOADED_KEY: str = 'total-len-loaded'
 SAVED_KEY: str = 'total-len-saved'
 NEXT_DICT_KEY: str = 'next-dict'
-MEAN_KEY: str = 'mean'
+N_COMPS_PER_FILE_KEY: str = 'n-comps-per-file'
 IDX_KEY: str = 'idx'
-TMP_DIR: str = 'tmp/'
+TMP_DIR: str = '.tmp/'
 
 
 def main():
@@ -38,29 +38,29 @@ def main():
         rmtree(TMP_DIR)
 
     mkdir(TMP_DIR)
-    mean_len: int = ceil(total_len / N_DICTS)
+    n_comps_per_file: int = ceil(total_len / N_OUTPUT_DICTS)
 
     alpha_filtered_dir: str = iterate_filtered_dicts(
-        alpha=alpha, func=add_save, use_p=True, mean_len=mean_len, local_vars=local_vars
+        alpha=alpha, func=add_save, use_p=True, n_comps_per_file=n_comps_per_file, local_vars=local_vars
     )
 
     next_dict: dict = local_vars[NEXT_DICT_KEY]
 
-    assert len(next_dict) <= mean_len
+    assert len(next_dict) <= n_comps_per_file
     assert local_vars[SAVED_KEY] <= local_vars[LOADED_KEY]
 
     save(next_dict=next_dict, local_vars=local_vars)
 
     assert local_vars[LOADED_KEY] == local_vars[SAVED_KEY]
     assert local_vars[LOADED_KEY] == total_len
-    assert len(listdir(TMP_DIR)) == N_DICTS
-    assert local_vars[IDX_KEY] == N_DICTS
+    assert len(listdir(TMP_DIR)) == N_OUTPUT_DICTS
+    assert local_vars[IDX_KEY] == N_OUTPUT_DICTS
 
     rmtree(alpha_filtered_dir)
     rename(src=TMP_DIR, dst=alpha_filtered_dir)
 
 
-def add_save(feat1: str, feat2: str, p: float, mean_len: int, local_vars: dict):
+def add_save(feat1: str, feat2: str, p: float, n_comps_per_file: int, local_vars: dict):
     """Adds the next comparison to the next dictionary and saves the next dictionary if it reaches average length"""
 
     local_vars[LOADED_KEY] += 1
@@ -68,7 +68,7 @@ def add_save(feat1: str, feat2: str, p: float, mean_len: int, local_vars: dict):
     key: tuple = get_comp_key(feat1=feat1, feat2=feat2)
     next_dict[key] = p
 
-    if len(next_dict) == mean_len:
+    if len(next_dict) == n_comps_per_file:
         save(next_dict=next_dict, local_vars=local_vars)
 
 
