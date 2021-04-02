6987c79d9d24f8f5f989952c92f77d8c5c184833
Erik Huckvale <erikhuck@gmail.com>
2021-04-01 19:21:25 -0600

    Adds the option to choose memory allocation to col comparison subset script

--- jobs/run-col-comparison-subset-jobs.sh
+++ jobs/run-col-comparison-subset-jobs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 SUBSET=$1
+MEM=$2
 
 for i in {0..3999}
 do
     echo $i
     echo $SUBSET
-    bash jobs/col-comparison-subset.submit $i $SUBSET
+    echo $MEM
+    bash jobs/col-comparison-subset.submit $i $SUBSET $MEM
 done
